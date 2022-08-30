d1cededa3b59f64c1cadeb91c944bf81bd4d7c00
Lautaro Vazquez <lauti1498@gmail.com>
2022-08-30 11:20:42 -0300

    branch del desafio

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+
+const CartWidget = () => {
+  return (
+    <LocalGroceryStoreIcon fontSize='large'/>
+  )
+}
+
+export default CartWidget
